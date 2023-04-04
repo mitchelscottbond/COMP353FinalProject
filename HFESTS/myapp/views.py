b011890aee2a143ed7f0ffe4bc7fd3fceb64143f
Mitchel Scott-Bond <mitchel.scottbond@gmail.com>
2023-04-04 18:31:08 -0400

    displaying queries to front end changes

--- HFESTS/myapp/views.py
+++ HFESTS/myapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+import pandas as pd
 from django.http import HttpResponse
 
 from myapp.models import Employee
@@ -12,18 +12,12 @@ def home(request):
     return render(request, 'home.html', {})
 
 def Query1(request):
-   # cursor = connection.cursor()
-    # Data retrieval operation - no commit required
-   # cursor.execute("SELECT employeeID FROM Employee")
-    #row = cursor.fetchone()
-    #print("This is row:" + row[0].employeeid)
-    employeeIDs = Employee.objects.raw("SELECT * FROM Employee")
-    for id in employeeIDs:
-        print(id.firstname)
-    print(employeeIDs[1].employeeid)
-    #for employee in (Employee.objects.raw("SELECT employeeID, FROM myapp_Employee")):
-    #    print(employee)
-    #print(row)
-    #print(employeeIDs)
-    return render (request, 'query1.html', {'employeeID':employeeIDs})
+
+    employeeIDs = Employee.objects.raw("SELECT employeeID, medicare FROM Employee ORDER BY employeeID DESC")
+
+    df = pd.DataFrame([item.__dict__ for item in employeeIDs])
+    df = df[df.columns[1:]]
+    print(df)
+
+    return render (request, 'query1.html', {'employees':df})
 	
